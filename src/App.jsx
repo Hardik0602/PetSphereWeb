342f36a6995568b98a0db5fdaaaa2105f5735b8d
Hardik <pathakhardik2004@gmail.com>
2025-07-30 09:49:38 +0530

    files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <text className='text-[40px] text-purple-600'>hi</text>
+    </>
+  )
+}
+
+export default App
